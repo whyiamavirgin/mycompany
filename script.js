90c5b8e148ad74ca2afb1c30c4695f68eaba4b45
whyiamavirgin <timofejadamia4@gmail.com>
2020-11-04 17:39:00 +0600

    финальная сборка

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ menu.onclick = function myFunction() {
         x.className = "topnav";
     }
 }
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth", 
+            block: "start"
+        })
+    })
+}
